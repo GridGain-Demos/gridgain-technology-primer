879eec6dae7b30aa6d8d94c2cac5e4f4b180bbd0
AnujaK <a.for.anuja@gmail.com>
2025-04-02 19:12:43 +0530

    Modified package names

--- core/sql/media_store_create.sql
+++ core/sql/media_store_create.sql
@@ -6,7 +6,7 @@ CREATE TABLE Artist
     ArtistId INT,
     Name VARCHAR(120),
     PRIMARY KEY (ArtistId)
-) WITH "template=partitioned, backups=1, CACHE_NAME=Artist, VALUE_TYPE=org.gridgain.model.Artist";
+) WITH "template=partitioned, backups=1, CACHE_NAME=Artist, VALUE_TYPE=com.gridgain.model.Artist";
 
 CREATE TABLE Album
 (
@@ -15,7 +15,7 @@ CREATE TABLE Album
     ArtistId INT,
     PRIMARY KEY (AlbumId, ArtistId)
 ) WITH "template=partitioned, backups=1, affinityKey=ArtistId, CACHE_NAME=Album,
-        KEY_TYPE=org.gridgain.model.AlbumKey, VALUE_TYPE=org.gridgain.model.Album";
+        KEY_TYPE=com.gridgain.model.AlbumKey, VALUE_TYPE=com.gridgain.model.Album";
 
 CREATE TABLE Customer
 (
@@ -33,7 +33,7 @@ CREATE TABLE Customer
     Email VARCHAR(60),
     SupportRepId INT,
     PRIMARY KEY (CustomerId)
-) WITH "template=partitioned, backups=1, CACHE_NAME=Customer, VALUE_TYPE=org.gridgain.model.Customer";
+) WITH "template=partitioned, backups=1, CACHE_NAME=Customer, VALUE_TYPE=com.gridgain.model.Customer";
 
 CREATE TABLE Invoice
 (
@@ -48,7 +48,7 @@ CREATE TABLE Invoice
     Total DECIMAL(10,2),
     PRIMARY KEY  (InvoiceId, CustomerId)
 ) WITH "template=partitioned, backups=1, affinityKey=CustomerId, CACHE_NAME=Invoice,
-        KEY_TYPE=org.gridgain.model.InvoiceKey, VALUE_TYPE=org.gridgain.model.Invoice";
+        KEY_TYPE=com.gridgain.model.InvoiceKey, VALUE_TYPE=com.gridgain.model.Invoice";
 
 CREATE TABLE InvoiceLine
 (
@@ -60,7 +60,7 @@ CREATE TABLE InvoiceLine
     Quantity INT,
     PRIMARY KEY (InvoiceLineId, CustomerId)
 ) WITH "template=partitioned, backups=1, affinityKey=CustomerId, CACHE_NAME=InvoiceLine,
-        KEY_TYPE=org.gridgain.model.InvoiceLineKey, VALUE_TYPE=org.gridgain.model.InvoiceLine";
+        KEY_TYPE=com.gridgain.model.InvoiceLineKey, VALUE_TYPE=com.gridgain.model.InvoiceLine";
 
 CREATE TABLE Employee
 (
@@ -80,28 +80,28 @@ CREATE TABLE Employee
     Fax VARCHAR(24),
     Email VARCHAR(60),
     PRIMARY KEY (EmployeeId)
-) WITH "template=partitioned, backups=1, CACHE_NAME=Employee, VALUE_TYPE=org.gridgain.model.Employee";
+) WITH "template=partitioned, backups=1, CACHE_NAME=Employee, VALUE_TYPE=com.gridgain.model.Employee";
 
 CREATE TABLE Genre
 (
     GenreId INT,
     Name VARCHAR(120),
     PRIMARY KEY (GenreId)
-) WITH "template=replicated, CACHE_NAME=Genre, VALUE_TYPE=org.gridgain.model.Genre";
+) WITH "template=replicated, CACHE_NAME=Genre, VALUE_TYPE=com.gridgain.model.Genre";
 
 CREATE TABLE MediaType
 (
     MediaTypeId INT,
     Name VARCHAR(120),
     PRIMARY KEY (MediaTypeId)
-) WITH "template=replicated, CACHE_NAME=MediaType, VALUE_TYPE=org.gridgain.model.MediaType";
+) WITH "template=replicated, CACHE_NAME=MediaType, VALUE_TYPE=com.gridgain.model.MediaType";
 
 CREATE TABLE Playlist
 (
     PlaylistId INT,
     Name VARCHAR(120),
     PRIMARY KEY  (PlaylistId)
-) WITH "template=partitioned, backups=1, CACHE_NAME=PlayList, VALUE_TYPE=org.gridgain.model.PlayList";
+) WITH "template=partitioned, backups=1, CACHE_NAME=PlayList, VALUE_TYPE=com.gridgain.model.PlayList";
 
 CREATE TABLE PlaylistTrack
 (
@@ -111,7 +111,7 @@ CREATE TABLE PlaylistTrack
     Dummy TINYINT, /* to fix the issue saying that the table must have at least one non-primary key column */
     PRIMARY KEY (PlaylistId, TrackId, ArtistId)
 ) WITH "template=partitioned, backups=1, affinityKey=ArtistId, CACHE_NAME=PlayListTrack,
-        KEY_TYPE=org.gridgain.model.PlaylistTrackKey, VALUE_TYPE=org.gridgain.model.PlayListTrack";
+        KEY_TYPE=com.gridgain.model.PlaylistTrackKey, VALUE_TYPE=com.gridgain.model.PlayListTrack";
 
 CREATE TABLE Track
 (
@@ -127,4 +127,4 @@ CREATE TABLE Track
     UnitPrice DECIMAL(10,2),
     PRIMARY KEY (TrackId)
 ) WITH "template=partitioned, backups=1, CACHE_NAME=Track,
-        KEY_TYPE=org.gridgain.model.TrackKey, VALUE_TYPE=org.gridgain.model.Track";
+        KEY_TYPE=com.gridgain.model.TrackKey, VALUE_TYPE=com.gridgain.model.Track";
